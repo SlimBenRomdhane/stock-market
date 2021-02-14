49bc198ceffe86a5cd0167adbadb93c06e879a99
SlimBenRomdhane <48137864+SlimBenRomdhane@users.noreply.github.com>
2021-02-14 21:38:49 +0100

    Adding Class Stock

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -1,3 +1,4 @@
+import { Stock } from './../../model/stock';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,22 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockItemComponent implements OnInit {
 
-  public name: string;
-  public code: string;
-  public price: number;
-  public previousPrice: number;
+  
+  public stock!: Stock;
 
-
-
-  constructor() {
-    this.name = 'Test Stock Company';
-    this.code = 'TSC';
-    this.price = 85;
-    this.previousPrice = 80;
-   }
+  constructor() {}
 
   ngOnInit() {
     
+    this.stock = new Stock('Test Stock Market', 'TSC',88,90);
+
+  }
+  toggleFavorite(event: any){
+    console.log('We are toggling the favorite state for this stock',event);
+    this.stock.favorite= !this.stock.favorite;
   }
 
 }
