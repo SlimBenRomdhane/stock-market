c8f5f9d9adb3e48baea394e9ea5b0eedd0a87f72
SlimBenRomdhane <48137864+SlimBenRomdhane@users.noreply.github.com>
2021-02-15 17:43:05 +0100

    Applying ngStyle

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -10,20 +10,25 @@ export class StockItemComponent implements OnInit {
 
   
   public stock!: Stock;
-  public stockClasses :any;
+  //public stockClasses :any;
+  public stockStyles :any;
 
   constructor() {}
 
   ngOnInit() {
     
-    this.stock = new Stock('Test Stock Market', 'TSC',100,90);
+    this.stock = new Stock('Test Stock Market', 'TSC',91.5,90);
     let diff = (this.stock.price / this.stock.previousPrice) - 1;
     let largeChange = Math.abs(diff) > 0.01;
-    this.stockClasses = {
-      "positive": this.stock.isPositiveChange(),
-      "negative": !this.stock.isPositiveChange(),
-      "large-change": largeChange,
-      "small-change": !largeChange
+    // this.stockClasses = {
+    //   "positive": this.stock.isPositiveChange(),
+    //   "negative": !this.stock.isPositiveChange(),
+    //   "large-change": largeChange,
+    //   "small-change": !largeChange
+    // };
+    this.stockStyles = {
+      "color": this.stock.isPositiveChange() ? "green" : "red",
+      "font-size": largeChange ? "1.2em" : "0.8em"
     };
 
   }
