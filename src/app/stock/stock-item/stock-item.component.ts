4c0ec65900c2d8ceff14114633b3d3487c16f5af
SlimBenRomdhane <48137864+SlimBenRomdhane@users.noreply.github.com>
2021-02-15 17:33:00 +0100

    Applying ngClass

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -10,12 +10,21 @@ export class StockItemComponent implements OnInit {
 
   
   public stock!: Stock;
+  public stockClasses :any;
 
   constructor() {}
 
   ngOnInit() {
     
-    this.stock = new Stock('Test Stock Market', 'TSC',88,90);
+    this.stock = new Stock('Test Stock Market', 'TSC',100,90);
+    let diff = (this.stock.price / this.stock.previousPrice) - 1;
+    let largeChange = Math.abs(diff) > 0.01;
+    this.stockClasses = {
+      "positive": this.stock.isPositiveChange(),
+      "negative": !this.stock.isPositiveChange(),
+      "large-change": largeChange,
+      "small-change": !largeChange
+    };
 
   }
   toggleFavorite(event: any){
